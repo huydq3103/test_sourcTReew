e413df8caaa984d8aa108bd96e51a8278bcdcab7
wanghuy031003 <huynobi1234@gmail.com>
2022-11-14 20:52:28 +0700

    firt commit

---
+++ testSorcTree/src/main/java/com/mycompany/testsorctree/test.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.testsorctree;
+
+/**
+ *
+ * @author Hp
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("ok test ");
+        System.out.println("branch cua develop1");
+        
+    }
+}
