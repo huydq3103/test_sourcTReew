f18e399c826f9f294d18cccfa5a1344becf2da96
wanghuy031003 <huynobi1234@gmail.com>
2022-11-14 20:57:25 +0700

    updaste main

--- testSorcTree/src/main/java/com/mycompany/testsorctree/test.java
+++ testSorcTree/src/main/java/com/mycompany/testsorctree/test.java
@@ -12,6 +12,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("ok test ");
         System.out.println("branch cua develop1");
-        
+        System.out.println("sajdhask");
     }
 }
